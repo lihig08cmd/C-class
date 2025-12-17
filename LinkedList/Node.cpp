b8f29ad1d4e55cfb9344a328d07a9a880a731464
Lihi Gavrielov <lihig08@gmail.com>
2025-12-17 11:10:19 -0800

    one

---
+++ LinkedList/Node.cpp
@@ -0,0 +1,9 @@
+// this is Node. cpp
+
+#include <iostream>
+using namespace std;
+
+#include "Node.h"
+
+//construscor
+Node::Node(Student*)
---
+++ LinkedList/Node.h
@@ -0,0 +1,34 @@
+// this is Node .h  //
+
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+using namespace std;
+
+class Node{
+ private:
+  // pointer to a student object
+  Student* student;
+  // pointer to next Node
+  Node* next;
+
+
+ public:
+  //Constructor
+  Node(Student*);
+
+  //destructor
+  ~Node();
+
+  // get next Node pointer
+  Node* getNext():
+
+  //get student pointer
+  Student* getStudent();
+
+  //set the next Node pointer
+  void setNext(Node*);
+
+};
+#endif
