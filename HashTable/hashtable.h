60de9b23c597174f8786337af7952e2c6a3ef1d7
Lihi Gavrielov <lihig08@gmail.com>
2026-02-24 10:35:56 -0800

    one

---
+++ HashTable/hashtable.h
@@ -0,0 +1,49 @@
+// this is hashtable .h
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <cstring>
+#include <iostrem>
+using namespace std;
+
+// this stract represents one student node
+struct Student{
+  char first[20];
+  char last[20];
+  int id;
+  float gpa;
+
+  // pointer to the next node
+  Student* next;
+
+};
+
+class HashTable{
+private:
+  //
+  Student** table;
+  int tableSize;
+  int nextID;
+
+  int hashFunction(int id);
+  void rehash();
+  int chainLength(Student* head);
+
+public:
+  // constructor / Destructor
+  HashTable();
+  ~HashTable();
+
+  // create student, Add, print, delete
+  Student* createStudent(const char* first, const char* last, float gpa);
+  void addStudent(Student* s);
+  void printTable();
+  void deleteStudent(int id);
+  
+  // random generator
+  void generateRandom(int count);
+  
+};
+#endif
+
---
+++ HashTable/main.cpp
@@ -0,0 +1,34 @@
+/*
+  Hash Table
+
+  lihi Gavrielov
+  2/ /2026   */
+
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+
+int main(){
+
+
+  while(true){
+    //user input
+    char input[80];
+    cout << "Enter command (ADD,PRINT,DELETE,QUIT, ____): " << endl;
+    cin>> input;
+
+    // if input is add
+    if(strcmp(input, "ADD")){
+    }
+
+
+
+  }
+
+
+
+}
