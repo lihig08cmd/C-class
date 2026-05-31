58d09fa1ea5e73f135304f355230219511afb13a
Lihi Gavrielov <lihig08@gmail.com>
2026-05-31 12:10:42 -0700

    first

---
+++ Graph/graph.h
@@ -0,0 +1,38 @@
+// this is graph.h
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class graph{
+ private:
+  // max amount of vertices
+  const int MAX = 20;
+
+  //stores vertex labels
+  char vertices[MAX];
+
+  //adjacency table
+  int adj[MAX][MAX];
+
+  //current number of vertices
+  int vertexCount;
+
+  //finds index of a vertex
+  int findVertex(char label[]);
+
+ public:
+  //constructor
+  graph();
+
+  void addVertex();
+  void addEdge();
+  void removeVertex();
+  void removeEdge();
+  void shortestPath();
+  void printGraph();
+
+};
+#endif
---
+++ Graph/main.cpp
@@ -0,0 +1,15 @@
+#include "Graph.h"
+
+int main(){
+  graph graph;
+
+  char command[20];
+  while(true){
+
+
+
+
+
+  }
+  return 0;
+}
